ea7c656416cb725394931333a13f1bafff62d78d
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2021-12-25 19:53:11 -0500

    Initial release.

---
+++ media/lua/server/Items/Distributions_df2a7746.lua
@@ -0,0 +1,237 @@
+Distributions = Distributions or {};
+
+local distributionTable = 
+{
+	all = 
+	{
+		restaurantdisplay = 
+		{
+			items = 
+			{
+			},
+		},
+		cashregister = 
+		{
+			items = 
+			{
+			},
+		},
+		campfire = 
+		{
+			items = 
+			{
+			},
+		},
+		clothingdryer = 
+		{
+			items = 
+			{
+			},
+		},
+		clothingwasher = 
+		{
+			items = 
+			{
+			},
+		},
+		freezer = 
+		{
+			items = 
+			{
+				"TurkeySlurpy.Turkey",2,
+				"TurkeySlurpy.Slurpy",1,
+			},
+		},
+		dishescabinet = 
+		{
+			items = 
+			{
+			},
+		},
+		dresser = 
+		{
+			items = 
+			{
+			},
+		},
+		postbox = 
+		{
+			items = 
+			{
+			},
+		},
+		corn = 
+		{
+			items = 
+			{
+			},
+		},
+		logs = 
+		{
+			items = 
+			{
+			},
+		},
+		fruitbusha = 
+		{
+			items = 
+			{
+			},
+		},
+		fruitbushb = 
+		{
+			items = 
+			{
+			},
+		},
+		fruitbushc = 
+		{
+			items = 
+			{
+			},
+		},
+		fruitbushd = 
+		{
+			items = 
+			{
+			},
+		},
+		fruitbushe = 
+		{
+			items = 
+			{
+			},
+		},
+		inventorymale = 
+		{
+			items = 
+			{
+			},
+		},
+		inventoryfemale = 
+		{
+			items = 
+			{
+			},
+		},
+		shelvesmag = 
+		{
+			items = 
+			{
+			},
+		},
+		desk = 
+		{
+			items = 
+			{
+			},
+		},
+		filingcabinet = 
+		{
+			items = 
+			{
+			},
+		},
+		stove = 
+		{
+			items = 
+			{
+			},
+		},
+		microwave = 
+		{
+			items = 
+			{
+				"TurkeySlurpy.Slurpy",1,
+			},
+		},
+		medicine = 
+		{
+			items = 
+			{
+			},
+		},
+		crate = 
+		{
+			items = 
+			{
+			},
+		},
+		counter = 
+		{
+			items = 
+			{
+			},
+		},
+		sidetable = 
+		{
+			items = 
+			{
+			},
+		},
+		fridge = 
+		{
+			items = 
+			{
+				"TurkeySlurpy.Turkey",3,
+				"TurkeySlurpy.Turkey",3,
+				"TurkeySlurpy.Slurpy",1,
+			},
+		},
+		vendingsnack = 
+		{
+			items = 
+			{
+			},
+		},
+		vendingpop = 
+		{
+			items = 
+			{
+			},
+		},
+		bin = 
+		{
+			items = 
+			{
+				"TurkeySlurpy.Slurpy",1,
+			},
+		},
+		officedrawers = 
+		{
+			items = 
+			{
+			},
+		},
+		metal_shelves = 
+		{
+			items = 
+			{
+			},
+		},
+		shelves = 
+		{
+			items = 
+			{
+			},
+		},
+		other = 
+		{
+			items = 
+			{
+			},
+		},
+	},
+	spiffosstorage = 
+	{
+		all = 
+		{
+			items = 
+			{
+				"TurkeySlurpy.Turkey",0,
+			},
+		},
+	},
+}
+
+table.insert(Distributions, distributionTable);
